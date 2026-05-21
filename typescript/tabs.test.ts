f347de90961b173355e93efe96483966c773d247
Gavin Stark <atthecodeface@gmail.com>
2026-05-21 12:08:37 +0100

    Changed tabs dramatically

--- typescript/tabs.test.ts
+++ typescript/tabs.test.ts
@@ -3,51 +3,59 @@
  */
 
 import * as html from "./html";
-import * as tabbed from "./tabbed";
+import * as tabs from "./tabs";
 
 declare global {
   var last_selected: string;
+  var last_selected_number: number;
 }
 global.last_selected = "";
+global.last_selected_number = 0;
 
 test("Basic", () => {
   const body = new html.HtmlElement(
     document.children[0]!.children[0]! as HTMLElement,
   );
   const div_tabs = body.add_ele("div", { id: "div-tabs" });
-  const ul = div_tabs.add_ele("ul", { id: "ul-tabs" });
-  ul.add_ele("li").add_ele("a", { tag_values: [["href", "#one"]] });
-  ul.add_ele("li").add_ele("a", { tag_values: [["href", "#two"]] });
-  ul.add_ele("li").add_ele("a", { tag_values: [["href", "#three"]] });
 
   const div_one = body.add_ele("div", { id: "one" });
   const div_two = body.add_ele("div", { id: "two" });
   const div_three = body.add_ele("div", { id: "three" });
 
-  const tabs = new tabbed.Tabs("#div-tabs", (hash) => {
-    global.last_selected = hash;
-  });
+  const doc_tabs = new tabs.Tabs(
+    "div-tabs",
+    (num, name) => {
+      global.last_selected = name;
+      global.last_selected_number = num;
+    },
+    [
+      ["one", 1],
+      ["two", 2],
+      ["three", 3],
+    ],
+  );
 
-  expect(tabs.select_hash("#one")).toBe(0);
-  expect(global.last_selected).toBe("#one");
-  expect(tabs.select_hash("#one")).toBe(0);
-  expect(global.last_selected).toBe("#one");
+  expect(doc_tabs.select("one")).toBe("one");
+  expect(global.last_selected).toBe("one");
+  expect(global.last_selected_number).toBe(1);
+  expect(doc_tabs.select("one")).toBe("one");
+  expect(global.last_selected).toBe("one");
   global.last_selected = "no callback invoked";
-  expect(tabs.select_hash("#one")).toBe(0);
+  expect(doc_tabs.select("one")).toBe("one");
   expect(global.last_selected).toBe("no callback invoked");
 
-  expect(tabs.select_hash("#two")).toBe(1);
-  expect(global.last_selected).toBe("#two");
-  expect(tabs.select_hash("#three")).toBe(2);
-  expect(global.last_selected).toBe("#three");
-  expect(tabs.select_hash("UNKNOWN")).toBe(null);
-  expect(global.last_selected).toBe("#three");
+  expect(doc_tabs.select("two")).toBe("two");
+  expect(global.last_selected).toBe("two");
+  expect(doc_tabs.select("three")).toBe("three");
+  expect(global.last_selected).toBe("three");
+  expect(doc_tabs.select("UNKNOWN")).toBe("one"); // Selects first declared tab on unknown name
+  expect(global.last_selected).toBe("one");
 
   expect(
     document
       .defaultView!.getComputedStyle(div_one.ele, null)
       .getPropertyValue("display"),
-  ).toBe("none");
+  ).toBe("block");
   expect(
     document
       .defaultView!.getComputedStyle(div_two.ele, null)
@@ -57,5 +65,5 @@ test("Basic", () => {
     document
       .defaultView!.getComputedStyle(div_three.ele, null)
       .getPropertyValue("display"),
-  ).toBe("block");
+  ).toBe("none");
 });
---
+++ typescript/tabs.ts
@@ -0,0 +1,185 @@
+/**
+ * History:
+ *
+ * 21 May:
+ *   Major rework from Tabbed to Tabs
+ *
+ * 13th April:
+ *
+ *   Converted from JavaScript
+ *
+ *   Requires a top level Tabs() creation from init complete
+ *
+ *   Tabbed.hash_change => select_hash; Tab.id => Tab.get_hash; Tab.select_tab => private
+ */
+
+import { HtmlElement } from "./html";
+
+/**
+ * A 'tab' in a tabbed page; this is private to the library
+ */
+class Tab<T> {
+  /** Client for the tab */
+  client: T;
+
+  /**
+   * The name of the 'div' that this tab corresponds to
+   *
+   * The div is hidden if this tab is not selected
+   */
+  name: string;
+
+  /**
+   *  The 'li' HTML element (which contains at least one 'a' element) for this tab
+   *
+   *  The 'className' attribute of this element will be "" if the tab is not selected, and "active" if it is selected
+   */
+  private li: HtmlElement;
+
+  /**
+   * The actual 'div' element that this corresponds to
+   */
+  private div: HtmlElement;
+
+  /**
+   * Create a new Tab given its parent, 'li' element and tab number
+   *
+   * The 'li' has at least one 'a' element in it, with an 'href' of '#tab-<id>',
+   * where 'tab-<id>' is the *id* of the div that contains the contents of that
+   * tab
+   */
+  constructor(li: HtmlElement, div: HtmlElement, name: string, client: T) {
+    this.li = li;
+    this.div = div;
+    this.name = name;
+    this.client = client;
+  }
+
+  /**
+   * Set the 'hidden' style for the div associated with this tab
+   */
+  set_hidden(hidden: boolean) {
+    this.div.ele.hidden = hidden;
+    if (hidden) {
+      this.li.ele.className = "";
+    } else {
+      this.li.ele.className = "active";
+    }
+  }
+}
+
+/**
+ * A class that handles a set of Tabs, only one of which should be selected, and that will become 'unhidden' while the others are 'hidden'
+ */
+export class Tabs<T> {
+  /**
+   * The set of Tab that this controls
+   */
+  tabs: Array<Tab<T>>;
+
+  /**
+   * the UL element that the tab list is
+   */
+  ul: HtmlElement;
+
+  /**
+   * The callback invoked when a tab is selected
+   */
+  callback: (t: T, id: string) => void;
+
+  /**
+   * The currently selected tab number
+   */
+  selected_tab: Tab<T> | null;
+
+  /**
+   * Create a new set of tabs whose tab list can be selected with 'container_select'
+   *
+   * This tab list must be an element that contains a 'ul' element, which in
+   * turn has 'li' for each tab, with each 'li' having an 'a' with an 'href'
+   * identifying the tab it is associated with.
+   */
+  constructor(
+    div_id: string,
+    tab_select_callback: (t: T, id: string) => void,
+    tabs: [[string, T]],
+  ) {
+    this.tabs = [];
+    this.callback = tab_select_callback;
+    this.selected_tab = null;
+
+    const tab_list = document.getElementById(div_id);
+    if (tab_list === null) {
+      throw new Error(`Failed to make 'Tabs' as ${div_id} was not found`);
+    }
+    this.ul = new HtmlElement(tab_list!).clear().add_ele("ul");
+
+    for (const [name, client] of tabs) {
+      this.add_tab(name, client);
+    }
+    window.addEventListener("hashchange", () => {
+      this.select(location.hash.slice(1));
+    });
+  }
+
+  add_tab(name: string, client: T) {
+    const href = "#" + name;
+    const div = document.querySelector(href);
+    if (div === null || !(div instanceof HTMLDivElement)) {
+      throw new Error(
+        `tab "$ref" does not have a relevant div in the document`,
+      );
+    }
+    const li = this.ul.add_ele("li");
+    const a = li.add_ele("a", {}, [["href", href]]);
+    const tab = new Tab(li, new HtmlElement(div), name, client);
+    this.tabs.push(tab);
+    a.ele.addEventListener("click", (e: Event) => {
+      this.select_tab(tab);
+      e.preventDefault();
+    });
+  }
+
+  tab(name: string): T | null {
+    for (const tab of this.tabs) {
+      if (name === tab.name) {
+        return tab.client;
+      }
+    }
+    return null;
+  }
+
+  /** Select the tab of the given name 'name'
+   *
+   * This will invoke the 'tab_select_callback' provided by the client with the
+   * selected tab's '#<tab-name>', if the tab number changes
+   *
+   *  Invoked when an <a href='#...'> link is selected
+   *
+   * @param {string} hash_name The '#' reference to follow
+   * @returns {number | undefined} The tab number selected, or null if the hash name was not known
+   */
+  select(name: string): string | null {
+    for (const t of this.tabs) {
+      if (name == t.name) {
+        return this.select_tab(t);
+      }
+    }
+    if (this.tabs.length === 0) {
+      return null;
+    }
+    return this.select_tab(this.tabs[0]!);
+  }
+
+  private select_tab(tab: Tab<T>): string {
+    if (tab === this.selected_tab) {
+      return tab.name;
+    }
+    for (const t of this.tabs) {
+      t.set_hidden(t !== tab);
+    }
+    this.selected_tab = tab;
+    this.callback(tab.client, tab.name);
+    return tab.name;
+  }
+}
